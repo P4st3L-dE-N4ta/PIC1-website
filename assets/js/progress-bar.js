d3abb0a38ce2ec3470747d902fc5b42225b6da02
P4st3L-dE-N4ta <rikirodrigues2004@gmail.com>
2025-05-12 22:27:24 +0100

    Added a rough progress line

---
+++ assets/js/progress-bar.js
@@ -0,0 +1,62 @@
+/*$(document).ready(function(){
+
+  
+  $('#update').click(function() {
+      var pb = new progressBar($('#input_progress').val(), $('#input_steps').val(),true);
+  });
+});*/
+
+$(document).ready(function() {
+  var pb = new progressBar(5, 7, true); // currentStep = 3, totalSteps = 6
+});
+
+/* todo. Turn into a prototype function */
+var progressBar = function (_step,_steps, _showSteps) {  
+  this.currentStep = _step;
+  this.totalSteps = _steps;
+  this.showSteps = false;
+  
+   $('.progress-wrapper').html('');
+  $('.progress-wrapper').append('<div class="progress-bar"></div><div class="steps-wrapper"><ul class="steps"></ul>');
+    
+  
+  for(i = 0; i != _steps; i ++)
+    {      
+      $(".steps").append("<li></li>");
+    }
+  
+  var steps = $('.steps li').length;  
+  var i = 0;
+  $.each($('.steps li'), function( index, value ) {    
+    if(_showSteps == true)
+      {    
+        var si = (_step -1);
+        const stepLabels = ["Problem Definition", "Initial Interviews", "Early Prototype", "Material Ordering", "Simulations", "Final Prototype", "Demonstration"]; // example labels
+        
+        if(index == si){
+          $(this).prepend('<span class="active">'+ (index + 1) + '</span>' + '<div class="step-label">' + stepLabels[index] + '</div>' +
+    '</div>');                    
+          }        
+          else if(index >= si)
+          {
+            $(this).prepend('<span>'+ (index + 1) + '</span>' + '</span>' + '<div class="step-label">' + stepLabels[index] + '</div>' +
+    '</div>' );
+          }
+          else if(index <= si)
+          {
+            $(this).prepend('<span class="halflings halflings-ok"></span>'+ '</span>' + '<div class="step-label">' + stepLabels[index] + '</div>' +
+    '</div>' );
+          }        
+      }    
+    i++;
+  });   
+  var g = (100 / i ).toFixed(4);  
+  var stepLength = $('.steps li').first().width();    
+  var pbLength = (_step >= steps) ? (g * _step) :  ((g * _step) - (g / 2));
+
+  $('.steps li').css("width", g + '%');    
+  $('.progress-bar').css("width", pbLength +"%");  
+  
+};
+progressBar.prototype.updateProgress = function() {  
+};
